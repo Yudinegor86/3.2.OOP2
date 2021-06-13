e278b6d6932ff5d28e8ca8653d883e4dea87599b
Yudin Egor <yudin.egor26@gmail.com>
2021-06-13 15:51:14 -0400

    initial commit

---
+++ src/main/java/ru/netology/domain/LikesInfo.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class LikesInfo {
+    private int count;
+    private boolean like;
+    private String iconLikeUrl;
+    private String iconIsNotLikeUrl;
+    //++getters/setters
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int authorId;
+    private String avatarUrl;
+    private String dateTime;
+    private String textPost;
+    private String urlImage;
+    private String comments;
+    private boolean isReposted;
+    private int repostCounter;
+    private int looksCounter;
+    private LikesInfo likesInfo;
+
+    //++getters/setters
+}
