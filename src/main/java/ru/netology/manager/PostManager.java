116b57388a27491929dd64d90e3d7fede372429a
Yudin Egor <yudin.egor26@gmail.com>
2021-06-13 17:54:48 -0400

    PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
